8b9dc3b936796a02e027e8689552342df2198e1b
Ahmed_Nazeer <ahmedgelany45@gmail.com>
2022-01-08 21:29:15 +0200

    enough here

--- MicroServices/Src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
+++ MicroServices/Src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
@@ -1,10 +1,10 @@
 ﻿using System.Threading.Tasks;
 using Dapper;
-using Discount.API.Entities;
+using Discount.Grpc.Entities;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
 
-namespace Discount.API.Repositories
+namespace Discount.Grpc.Repositories
 {
     public class DiscountRepository:IDiscountRepository
     {
